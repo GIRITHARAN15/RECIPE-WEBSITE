b3dd162bd07bf261cad383aa5b7108fd1848ed6c
GIRITHARAN15 <115262885+GIRITHARAN15@users.noreply.github.com>
2022-10-19 22:38:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Meal from './components/meal';
+import './components/style.css';
+import {Routes,Route} from "react-router-dom";
+import RecipeInfo from './components/RecipeInfo';
+function App() {
+  return (
+    <>
+    <Routes>
+    <Route path="/" element={<Meal/>}/>
+    <Route path="/:MealId" element={<RecipeInfo/>}/>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
